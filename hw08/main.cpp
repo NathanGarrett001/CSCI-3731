e0f2df9075b05d3f3dbab95b4e80fb2b021b653c
NathanGarrett001 <54726582+NathanGarrett001@users.noreply.github.com>
2022-03-26 13:43:46 -1000

    Update main.cpp

--- hw08/main.cpp
+++ hw08/main.cpp
@@ -1,5 +1,6 @@
 #include "vector.h"
 #include "planet.h"
+#include "satellite.h"
 #include <iostream>
 
 int main()
@@ -7,6 +8,25 @@ int main()
 	auto x = 0.0;
 	auto y = 0.0;
 	auto z = 0.0;
-	vector v3;
+	
+
+	vector yPos(0.0, 400000.0, 0.0);
+	vector xVel(7660.0, 0.0, 0.0);
+	vector zAccel(0.0, 0.0, 0.0);
+	planet earth(5972000000000000000000000.0);
+	satellite iss(yPos, xVel, earth);
+	bool crash = false;
+	int timeInt = 1;
+
+	while (!crash) {
+
+		crash = earth.accel(yPos, zAccel);
+
+		iss.update(yPos, zAccel, xVel, timeInt);
+
+
+
+
+	}
 
 }
