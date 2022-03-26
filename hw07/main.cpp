d0b9fb6b543aa4df192310d58c238fb9bbb3a922
NathanGarrett001 <54726582+NathanGarrett001@users.noreply.github.com>
2022-03-25 23:41:09 -1000

    Update main.cpp

--- hw07/main.cpp
+++ hw07/main.cpp
@@ -8,36 +8,37 @@ int main()
 	auto z = 0.0;
 	vector v3;
 
-    std::cout << "Welcome to the Vectornator 9000!"<<std::endl;
+        std::cout << "Welcome to the Vectornator 9000!"<<std::endl;
 	std::cout << "Please enter your first vector points"<<std::endl;
 	std::cin >> x >> y >> z;
 
-	vector v1(&x, &y, &z);
+	vector v1(x, y, z);
 
 	std::cout << "Please enter your second vector points" << std::endl;
-    std::cin >> x >> y >> z;
+        std::cin >> x >> y >> z;
 
-	vector v2(&x, &y, &z);
+	vector v2(x, y, z);
 
 	std::cout << "First I will add the two vectors." << std::endl;
-	v3.addVect(v1, v2, &x, &y, &z);
+	v3 = v1.operator+(v2);
 	std::cout << "You have a new vector now and its coordinates are : x = " << v3.getX() << ", y = " << v3.getY() << ", z = " << v3.getZ() << std::endl;
 	
 	std::cout << "Now I will subtract the two original vectors." << std::endl;
-	v3.subVect(v1, v2, &x, &y, &z);
-	std::cout << "Your subtracted vector coordinates are : x = " << x << ", y = " << y << ", z = " << z << std::endl;
+	v3 = v1.operator-(v2);
+	std::cout << "Your subtracted vector coordinates are : x = " << v3.getX() << ", y = " << v3.getY() << ", z = " << v3.getZ() << std::endl;
 
 	std::cout << "Next is to use the += operator to increase the values of a vector by one." << std::endl;
 	v1.operator+=(v1);
 	std::cout << "The new points for your first vector are : x = " << v1.getX() << ", y =  " << v1.getY() << ", z = " << v1.getZ() << "." << std::endl;
 
 	std::cout << "Next we will multiply a vector by a double, 2.0" << std::endl;
-	v1.times2Vect(v1, &x, &y, &z);
-	std::cout << "The new points for your first vector are : x = " << v1.getX() << ", y = " << v1.getY() << ", z = " << v1.getZ() << "." << std::endl;
+	v3 = v3.operator*(v1);
+	std::cout << "The new points for your first vector are : x = " << v3.getX() << ", y = " << v3.getY() << ", z = " << v3.getZ() << "." << std::endl;
 
 	std::cout << "Finally, we will now multiply the two vectors with each other!" << std::endl;
 	std::cout << "REMINDER, current values for first vector: x=" << v1.getX() << ", y=" << v1.getY() << ", z=" << v1.getZ() << "." << std::endl;
 	std::cout << "REMINDER, current values for second vector: x=" << v2.getX() << ", y=" << v2.getY() << ", z=" << v2.getZ() << "." << std::endl;
-	std::cout << "The dot Product of your two pints is " << v3.multVects(v1, v2, &x, &y, &z) << ". " << std::endl;
+	std::cout << "The dot Product of your two points is " << v3.multVects(v1, v2) << ". " << std::endl;
 
 }
+
