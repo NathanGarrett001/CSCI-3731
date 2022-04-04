c6174a49f4045216275478ce5118f6151987e0ce
NathanGarrett001 <54726582+NathanGarrett001@users.noreply.github.com>
2022-04-03 19:01:48 -1000

    Update planet.h

--- hw08/planet.h
+++ hw08/planet.h
@@ -1,26 +1,24 @@
 
 #ifndef PLANET_H
 #define PLANET_H
+#include "vector.h"
 
-class vector;
-class satellite;
 
 class planet {
 
 private:
-
-	double m = 0.0;
-	double r = 0.0;
-	vector* yPos = nullptr;
-	vector* zAccel = nullptr;
+    const double G = 0.0000000000667408;
+	double M = 5972000000000000000000000.0;
+	double radius = 6378000.0;
+	vector* r = nullptr;
 
 public:
 	planet();
-	planet(double m);
+	planet(double m, double radius);
 	virtual ~planet();
 
 	double getR() const;
-	bool accel(vector& yPos, vector& zAccel);
+	bool accel(const vector& r, vector& accel) const;
 
 };
 
