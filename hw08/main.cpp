f0e4d2fe715bc93443a5c910e1fe188b89d1aa37
NathanGarrett001 <54726582+NathanGarrett001@users.noreply.github.com>
2022-04-03 19:00:34 -1000

    Update main.cpp

--- hw08/main.cpp
+++ hw08/main.cpp
@@ -10,22 +10,17 @@ int main()
 	auto z = 0.0;
 	
 
-	vector yPos(0.0, 400000.0, 0.0);
-	vector xVel(7660.0, 0.0, 0.0);
-	vector zAccel(0.0, 0.0, 0.0);
-	planet earth(5972000000000000000000000.0);
-	satellite iss(yPos, xVel, earth);
+	vector position(0.0, (400000.0 + 6378000.0), 0.0);
+	vector velocity(7660.0, 0.0, 0.0);
+	planet earth(5972000000000000000000000.0, 6378000.0);
+	satellite iss(position, velocity, earth);
 	bool crash = false;
 	int timeInt = 1;
 
-	while (!crash) {
-
-		crash = earth.accel(yPos, zAccel);
-
-		iss.update(yPos, zAccel, xVel, timeInt);
-
-		std::cout << "Your position in space is:  Altitude - " << yPos.getY() << ", velocity - " << xVel.getX() << ". " << std::endl;
-
+	while (iss.update(timeInt)) {
+		std::cout << "Position = " << iss.getPosition() << ", Velocity = " << iss.getVelocity() << "" << std::endl;
 	}
+
+	std::cout << "You crashed" << std::endl;
 
 }
